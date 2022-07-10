e2d317d4407cec8e65375afcbc29a9c00ea96e8f
Alexandre Ortiz <alexandre.ortiz49@gmail.com>
2022-07-10 18:17:37 +0200

    utilisation Promesse

--- routes/books.js
+++ routes/books.js
@@ -9,37 +9,37 @@ const url = 'https://www.leslibraires.fr/recherche/?q='
 router.get('/get', function(req, res, next) {
 	const ean = req.query.ean
 	if(typeof ean != 'undefined' || ean  !== ""){
-		axios(url + ean)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            var book = {
-            	"name" : undefined,
-            	"author" : undefined,
-            	"price" : undefined
-            }
-
-            $('div.main-infos', html).each(function () { //<-- cannot be a function expression
-                const title = $(this).text()
-                book.name = $(this).find('h1').find('span').text()
-                book.author = $(this).find('h2').find('a').text()
-            })
-            $('div.offer', html).each(function () { //<-- cannot be a function expression
-                book.price = $(this).find('span.price').text()
-            })
-
-            if(typeof book.name != 'undefined' ){
-            	res.json({book})
-            }else{
-            	res.json({"status" : 400,
-					"message" : "Book not find"})
-            }
-        }).catch(err => console.log(err))
+		getBookByEan(ean)
+			.then(book => res.json({book}))
+			
 	}else{
 		res.json({"status" : 400,
 			"message" : "EAN is required"})
 	}
-	
 });
+
+function getBookByEan(ean) {
+    return axios(url + ean)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            var book = {
+                "name": undefined,
+                "author": undefined,
+                "price": undefined
+            }
+
+            $('div.main-infos', html).each(function() {
+                const title = $(this).text()
+                book.name = $(this).find('h1').find('span').text()
+                book.author = $(this).find('h2').find('a').text()
+            })
+
+            $('div.offer', html).each(function() {
+                book.price = $(this).find('span.price').text()
+            })
+            return book
+        }).catch(err => console.log(err))
+}
 
 module.exports = router;
